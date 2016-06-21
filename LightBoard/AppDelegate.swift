48dbfdbe8ad8a7d1b27abdf945f15494d56e9e84
pochy <kenta.teruteru@gmail.com>
2016-06-21 14:06:10 +0900

    Initial Commit

---
+++ LightBoard/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  LightBoard
+//
+//  Created by Nakajima Kenta on 2016/06/21.
+//  Copyright © 2016年 Nakajima Kenta. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ LightBoard/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  LightBoard
+//
+//  Created by Nakajima Kenta on 2016/06/21.
+//  Copyright © 2016年 Nakajima Kenta. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
